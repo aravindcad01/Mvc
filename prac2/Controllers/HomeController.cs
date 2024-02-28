5c09df04e3804c794441fccc97358c6e04caa2ff
aravindcad01 <aravind2k23cad01@gmail.com>
2024-02-29 00:47:54 +0530

    29/02/24

---
+++ prac2/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using prac2.Models;
+
+namespace prac2.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Help()
+    {
+        return View();
+    }
+    public IActionResult contact()
+    {
+        return View();
+    }
+    public IActionResult login()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
